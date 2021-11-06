426ecc19d38508ec75e8eb516b73ca4644fc844b
NickTacke <nicktacke@icloud.com>
2021-11-06 21:26:23 +0100

    Minor fixes

--- contact/submit.php
+++ contact/submit.php
@@ -1,3 +1,14 @@
 <?php
-echo $_POST["name"];
+$body = file_get_contents("php://input");
+
+$object = json_decode($body, true);
+
+$naam = $object["naam"];
+$email = $object["email"];
+$telefoon = $object["telefoon"];
+$omschrijving = $object["omschrijving"];
+
+$msg = "Naam: " . $naam . "\nEmail: " . $email . "\nTelefoon: " . $telefoon . "\n\n" . $omschrijving;
+
+mail("info@ntps.nl", "New message", $msg);
 ?>
